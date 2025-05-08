5f6956dd75a93bc5736be6341dca0f5735f7d82c
Melipilla <melipilla@duoc.acad>
2025-05-08 15:18:24 -0400

    cree carpeta cliente y inventario

---
+++ src/main/java/com/perfulandia/perfulandia/Cliente/ClienteController.java
@@ -0,0 +1,4 @@
+package com.perfulandia.perfulandia.Cliente;
+
+public class ClienteController {
+}
---
+++ src/main/java/com/perfulandia/perfulandia/Inventario/InventarioController.java
@@ -0,0 +1,5 @@
+package com.perfulandia.perfulandia.Inventario;
+
+public class InventarioController {
+
+}
