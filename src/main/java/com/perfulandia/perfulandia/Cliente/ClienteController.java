0e2f83d8a7d6b3e758618ab1b8fcb130e5fffaad
Melipilla <melipilla@duoc.acad>
2025-05-08 15:26:51 -0400

    holanda

--- src/main/java/com/perfulandia/perfulandia/Cliente/ClienteController.java
+++ src/main/java/com/perfulandia/perfulandia/Cliente/ClienteController.java
@@ -1,4 +1,17 @@
 package com.perfulandia.perfulandia.Cliente;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClienteController {
+@Column(unique = true,length = 13 , nullable = false)
+    private String run ;
+
 }
