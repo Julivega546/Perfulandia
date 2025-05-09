b1fcbf73821351fa4a443fbd4c33f16bacb1a006
Melipilla <melipilla@duoc.acad>
2025-05-09 14:42:08 -0400

    O_O

--- src/main/java/com/perfulandia/perfulandia/repository/ClienteRepository.java
+++ src/main/java/com/perfulandia/perfulandia/repository/ClienteRepository.java
@@ -2,7 +2,11 @@ package com.perfulandia.perfulandia.repository;
 
 import com.perfulandia.perfulandia.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import  org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
