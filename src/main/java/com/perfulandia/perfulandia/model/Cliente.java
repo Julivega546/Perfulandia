392e95d9f2e033a66a8e297bcfd0b2ac558e632c
Melipilla <melipilla@duoc.acad>
2025-05-09 14:07:36 -0400

    cliente listo

--- src/main/java/com/perfulandia/perfulandia/model/Cliente.java
+++ src/main/java/com/perfulandia/perfulandia/model/Cliente.java
@@ -1,4 +1,23 @@
 package com.perfulandia.perfulandia.model;
 
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
+     @Column(unique = true , length = 13 , nullable = false)
+    private String run;
+
+     @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = false)
+    private String correo;
 }
