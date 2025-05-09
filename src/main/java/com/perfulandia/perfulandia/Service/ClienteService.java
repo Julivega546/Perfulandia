38874951ff1e6e3758d6f34fd66a50c947d69780
Melipilla <melipilla@duoc.acad>
2025-05-09 14:56:41 -0400

    service cliente

---
+++ src/main/java/com/perfulandia/perfulandia/Service/ClienteService.java
@@ -0,0 +1,31 @@
+package com.perfulandia.perfulandia.Service;
+
+import com.perfulandia.perfulandia.model.Cliente;
+import com.perfulandia.perfulandia.repository.ClienteRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ClienteService {
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> findAll(){
+    return clienteRepository.findAll();
+    }
+    public Cliente findById(long id){
+        return clienteRepository.findById(id).get();
+    }
+
+    public Cliente save(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    public void delete(long id){
+        clienteRepository.deleteById(id);
+    }
+}
