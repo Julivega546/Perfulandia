f1e1815a7a2e8e44a6ce71502487220d856bcf43
Melipilla <melipilla@duoc.acad>
2025-05-09 15:30:14 -0400

    ziiiiiiii

--- src/main/java/com/perfulandia/perfulandia/Controller/ClienteController.java
+++ src/main/java/com/perfulandia/perfulandia/Controller/ClienteController.java
@@ -6,9 +6,7 @@ import com.perfulandia.perfulandia.model.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,4 +26,18 @@ public class ClienteController {
         return ResponseEntity.ok(clientes);
     }
 
+    @PostMapping
+    public ResponseEntity<Cliente> guardar(@RequestBody Cliente cliente){
+        Cliente clienteNuevo =clienteService.save(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteNuevo);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Integer id){
+        try{
+            Cliente cliente = clienteService.findById(id);
+            return ResponseEntity.ok(cliente);
+        } catch ( Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
