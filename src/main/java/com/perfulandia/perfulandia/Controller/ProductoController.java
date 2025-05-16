303b20caaa4921066433626854282a624d65b115
Melipilla <melipilla@duoc.acad>
2025-05-16 15:35:06 -0400

    holo

--- src/main/java/com/perfulandia/perfulandia/Controller/ProductoController.java
+++ src/main/java/com/perfulandia/perfulandia/Controller/ProductoController.java
@@ -51,6 +51,7 @@ public class ProductoController{
             productonuevo.setId(productonuevo.getId());
             productonuevo.setPrecio(productonuevo.getPrecio());
             productonuevo.setNombre(productonuevo.getNombre());
+            productonuevo.setStock(productonuevo.getStock());
 
 
             productoService.save(productonuevo);
